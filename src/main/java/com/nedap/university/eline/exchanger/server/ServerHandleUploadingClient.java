61bd77d5a54e91acb8da298d1020d35e1ab09a1f
Eline Verbon <eline.verbon@nedap.com>
2020-04-13 20:46:59 +0200

    Add a ServerHandleUploadingClient class which is called from the Server when the clients indicates to want to upload a file. It sends a response to the original choice message of the client and starts the FileReceiveManager with an absoluteFilePath for the to-be-uploaded file so it can be saved directly.

---
+++ src/main/java/com/nedap/university/eline/exchanger/server/ServerHandleUploadingClient.java
@@ -0,0 +1,37 @@
+package com.nedap.university.eline.exchanger.server;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Arrays;
+
+import com.nedap.university.eline.exchanger.manager.FileReceiveManager;
+
+public class ServerHandleUploadingClient {
+
+    public void letUserUploadFile(final DatagramPacket packet) {
+    	try {
+    		final InetAddress clientAddress = packet.getAddress();
+	    	final int clientPort = packet.getPort();
+	    	byte[] choiceByte = Arrays.copyOfRange(packet.getData(), 0, 1);
+	    	new DatagramPacket(choiceByte, choiceByte.length, clientAddress, clientPort);
+	    	DatagramSocket thisCommunicationsSocket = new DatagramSocket();
+			thisCommunicationsSocket.send(new DatagramPacket(choiceByte, choiceByte.length, clientAddress, clientPort));
+			
+			byte[] fileNameBytes = Arrays.copyOfRange(packet.getData(), 1, packet.getLength());
+			String fileName = new String(fileNameBytes);
+			System.out.println(fileName);
+			
+			String absoluteFilePath = System.getProperty ("user.home") + "/Desktop/" + fileName;
+			new FileReceiveManager(thisCommunicationsSocket, absoluteFilePath).receiveFile();
+//	    	String absoluteFilePath = "/home/pi/fileLocalTestUpload.pdf";
+	    	
+	        
+	        thisCommunicationsSocket.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+}
