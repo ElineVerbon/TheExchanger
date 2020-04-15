4c0e999153349458f1b4489ab57d685ebc7f7158
Eline Verbon <eline.verbon@nedap.com>
2020-04-15 19:40:12 +0200

    Replace AbstractClientExectur by two other classes (ChoiceCommunicator and FileChooser) to separate tasks. Other classes do not inherit, but get/make instances of these classes upon construction.

---
+++ src/main/java/com/nedap/university/eline/exchanger/client/ChoiceCommunicator.java
@@ -0,0 +1,66 @@
+package com.nedap.university.eline.exchanger.client;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+
+public class ChoiceCommunicator {
+	private int generalServerPort;
+	private InetAddress serverAddress;
+	
+	public ChoiceCommunicator(int serverPort, InetAddress serverAddress) {
+		this.generalServerPort = serverPort;
+		this.serverAddress = serverAddress;
+	}
+	
+	public int getGeneralServerPort() {
+		return generalServerPort;
+	}
+	
+	public InetAddress getServerAddress() {
+		return serverAddress;
+	}
+	
+	public DatagramPacket communicateChoiceToServer(final byte[] choiceByte, final byte[] dataBytes, final DatagramSocket socket) throws SocketTimeoutException {
+    	DatagramPacket packet = makeDataPacket(choiceByte, dataBytes, serverAddress, generalServerPort);
+    	sendToServer(packet, socket);
+    	DatagramPacket response = receivePacket(socket);
+    	return response;
+    }
+    
+    private DatagramPacket makeDataPacket(final byte[] choiceByte, final byte[] dataBytes, 
+    		final InetAddress serverAddress, final int serverPort) {
+		byte[] packetBytes = new byte[dataBytes.length + choiceByte.length];
+		System.arraycopy(choiceByte, 0, packetBytes, 0, choiceByte.length);
+		System.arraycopy(dataBytes, 0, packetBytes, choiceByte.length, dataBytes.length);
+		return new DatagramPacket(packetBytes, packetBytes.length, serverAddress, serverPort);
+	}
+    
+    
+    private DatagramPacket receivePacket(DatagramSocket socket) throws SocketTimeoutException {
+		DatagramPacket response = null;
+    	try {
+    		response = new DatagramPacket(new byte[1], 1);
+			socket.setSoTimeout(2000);
+			socket.receive(response);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			
+		return response;
+    }
+    
+    private void sendToServer(DatagramPacket packet, DatagramSocket socket) {
+    	try {
+			socket.send(packet);
+		} catch (IOException e) {
+			System.out.println("Could not send the choice to the server.");
+		}
+    }
+
+}
---
+++ src/main/java/com/nedap/university/eline/exchanger/client/FileChooser.java
@@ -0,0 +1,71 @@
+package com.nedap.university.eline.exchanger.client;
+
+import java.io.File;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.nedap.university.eline.exchanger.communication.CommunicationStrings;
+import com.nedap.university.eline.exchanger.exceptions.UserQuitToMainMenuException;
+
+public class FileChooser {
+	
+	private List<String> savedFileNames;
+	
+	public FileChooser() {
+		savedFileNames = new ArrayList<>();
+	}
+    
+    public File getUserSelectedLocalFile(final String message) throws UserQuitToMainMenuException {
+    	ClientTUI.showMessage(message);
+    	String absoluteFilePath = ClientTUI.getString();
+    	checkForExit(absoluteFilePath);
+    	File file = new File(absoluteFilePath);
+
+    	while (!(file.exists())) {
+    		ClientTUI.showMessage("The file could not be found. Please try again. (Type x to return to the main menu.)");
+    		absoluteFilePath = ClientTUI.getString();
+    		checkForExit(absoluteFilePath);
+        	file = new File(absoluteFilePath);
+    	}
+    	
+    	return file;
+    }
+
+    public String letUserEnterTheNameOfAFileOnTheServer(final String message, final ClientListAsker listAsker) throws UserQuitToMainMenuException, SocketTimeoutException {
+		ClientTUI.showMessage("We will first download all the files on the server.");
+		String fileNamesOnServer = listAsker.letClientAskForList();
+    	ClientTUI.showMessage(message);
+    	
+    	List<String> fileNames = Arrays.asList(fileNamesOnServer.split(CommunicationStrings.SEPARATION_TWO_FILES));
+    	final String fileName = getValidInput(fileNames);
+    	savedFileNames = fileNames;
+    	return fileName;
+    }
+    
+    //can only call this after having called the method above & within the ClientDownloader. (Don't know whether I can enforce that in some way.)
+    public String letUserEnterAnotherName() throws UserQuitToMainMenuException {
+    	ClientTUI.showMessage("Please enter another file name.");
+    	return getValidInput(savedFileNames);
+    }
+    
+    private String getValidInput(final List<String> validFileNames) throws UserQuitToMainMenuException {
+    	String fileName = ClientTUI.getString();
+    	checkForExit(fileName);
+    	
+    	while (!(validFileNames.contains(fileName)) || fileName.equals("")) {
+    		ClientTUI.showMessage("The file name is not known on the server. Please try again. (Type x to return to the main menu.)");
+    		fileName = ClientTUI.getString();
+    		checkForExit(fileName);
+    	}
+    	
+    	return fileName;
+    }
+    
+    private void checkForExit(final String string) throws UserQuitToMainMenuException {
+    	if (string.equals("x")) {
+    		throw new UserQuitToMainMenuException();
+    	}
+    }
+}
